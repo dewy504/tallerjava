4ec174dd95af9b039bd79071217b70e674da855e
Dewys Eguis <dewys0507@gmail.com>
2021-05-26 20:05:32 -0500

    armstrong-numbers

---
+++ armstrong-numbers/armstrong-numbers.js
@@ -0,0 +1,18 @@
+//
+import { String } from "core-js";
+
+export const isArmstrongNumber = (number) => {
+  
+  let numbers = String(number).split('');
+  let acumulator = 0;
+
+  for(let i = 0; i< numbers.length; i++){
+    numbers[i] = parseInt(numbers[i]);
+  }
+
+  for(let i = 0; i < numbers.length; i++) {
+    acumulator += numbers[i]**numbers.length;
+  }
+
+  return acumulator == number;
+};
